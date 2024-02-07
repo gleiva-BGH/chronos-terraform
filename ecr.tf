c5f5fcc051b02c8a574b271321cdba67127bd04a
gaston.leiva <74213975+gastonsj@users.noreply.github.com>
2024-02-07 00:07:14 -0300

    add ecr lifecycle

--- ecr.tf
+++ ecr.tf
@@ -6,3 +6,8 @@ resource "aws_ecr_repository" "api" {
     scan_on_push = false
   }
 }
+resource "aws_ecr_lifecycle_policy" "api" {
+  repository = aws_ecr_repository.api.name
+
+  policy = file("policies/ecr-lifecycle-policy-expire-untagged.json")
+}
